320d883163132ef4905f9f5fe80a2506a502a035
DevMa7e1 <thedevma7e1@gmail.com>
2023-12-28 13:30:33 +0200

    Add path security with werkzeug.utils's secure_filename function.

--- folderbase.py
+++ folderbase.py
@@ -1,21 +1,23 @@
-import os
+import os, werkzeug.utils
+
 def write(name : str, data : str):
-	f = open(f"{os. getcwd()}/folderbase/{name}", 'w')
+	
+	f = open(f"{os. getcwd()}/folderbase/{werkzeug.utils.secure_filename(name)}", 'w')
 	f.write(data)
 	f.close()
 def write(name, data : int):
-	f = open(f"{os. getcwd()}/folderbase/{name}", 'w')
+	f = open(f"{os. getcwd()}/folderbase/{werkzeug.utils.secure_filename(name)}", 'w')
 	f.write(str(data))
 	f.close()
 def ishere(name):
-	return os.path.exists(f"{os. getcwd()}/folderbase/{name}")
+	return os.path.exists(f"{os. getcwd()}/folderbase/{werkzeug.utils.secure_filename(name)}")
 def read(name):
 	if ishere(name):
-		f = open(f"{os. getcwd()}/folderbase/{name}", 'r')
+		f = open(f"{os. getcwd()}/folderbase/{werkzeug.utils.secure_filename(name)}", 'r')
 		r = f.read()
 		f.close()
 		return r
 	else:
 		return False
 def delete(name):
-	os.remove(f"{os. getcwd()}/folderbase/{name}")
+	os.remove(f"{os. getcwd()}/folderbase/{werkzeug.utils.secure_filename(name)}")
