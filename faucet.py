caf1eba9bc97edf4e62172dda20bfc2ea8a239d2
DevMa7e1 <thedevma7e1@gmail.com>
2024-03-22 19:03:43 +0200

    Made it so if you miss an hour, it will still give you the one coin reward all in one transaction.

--- faucet.py
+++ faucet.py
@@ -4,7 +4,7 @@ import time, folderbase
 def drop(wallet):
     if folderbase.ishere(wallet+"_faucet"):
         if float(folderbase.read(wallet+'_faucet')) < time.time():
-            give(wallet, 1)
+            give(wallet, int((time.time() - float(folderbase.read(wallet+"_faucet")))/3600))
             folderbase.write(wallet+"_faucet", time.time()+(60*60))
             return 'OK GOOD'
         else:
